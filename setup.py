135fdd541a38aaf0a82fdca66302a3e74f65f205
snare <snare@ho.ax>
2015-02-26 21:18:59 +1100

    Version bump to 0.3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name = "scruffy",
-    version = "0.2.2",
+    version = "0.3",
     author = "snare",
     author_email = "snare@ho.ax",
     description = ("The janitor"),
     license = "Buy snare a beer",
     keywords = "scruffy",
     url = "https://github.com/snare/scruffy",
-    packages=['scruffy', 'sqlite3'],
-    install_requires = ['pyyaml'],
+    packages=['scruffy'],
+    install_requires = ['pyyaml', 'sqlalchemy', 'six'],
 )
