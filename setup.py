4b62930e6fee5703ebd3801cd2a822179b2859b4
snare <snare@ho.ax>
2016-03-15 23:54:17 +1100

    Add license

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name = "scruffington",
-    version = "0.3.1",
-    author = "snare",
-    author_email = "snare@ho.ax",
-    description = ("The janitor"),
-    license = "Buy snare a beer",
-    keywords = "scruffy",
-    url = "https://github.com/snare/scruffy",
+    name="scruffington",
+    version="0.3.1",
+    author="snare",
+    author_email="snare@ho.ax",
+    description=("The janitor"),
+    license="MIT",
+    keywords="scruffy",
+    url="https://github.com/snare/scruffy",
     packages=['scruffy'],
-    install_requires = ['pyyaml', 'sqlalchemy', 'six'],
+    install_requires=['pyyaml', 'sqlalchemy', 'six'],
 )
