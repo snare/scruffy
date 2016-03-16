a09afd24d74036236584ffadaddd58c023e6c6db
snare <snare@ho.ax>
2016-03-16 20:25:45 +1100

    Added __all__

--- scruffy/__init__.py
+++ scruffy/__init__.py
@@ -4,3 +4,12 @@ from .file import File, LogFile, LockFile
 from .plugin import PluginRegistry, Plugin, PluginManager
 from .config import ConfigNode, Config, ConfigEnv, ConfigFile, ConfigApplicator
 from .state import State
+
+__all__ = [
+    "Environment",
+    "Directory", "PluginDirectory", "PackageDirectory", "PackageFile",
+    "File", "LogFile", "LockFile",
+    "PluginRegistry", "Plugin", "PluginManager",
+    "ConfigNode", "Config", "ConfigEnv", "ConfigFile", "ConfigApplicator",
+    "State"
+]
