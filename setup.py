2105502ef2f572e4ab1afe7d77e2fdf9f5e8be39
Zachary Cutlip <uid000@gmail.com>
2019-10-02 10:48:00 -0700

    add long_description from README.rst

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open("README.rst", "r") as fp:
+    long_description = fp.read()
+
 setup(
     name="scruffington",
     version="0.3.8.1",
     author="snare",
     author_email="snare@ho.ax",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     description=("The janitor"),
     license="MIT",
     keywords="scruffy",
